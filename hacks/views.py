ce9f2e4ec9b231de53fddbd6d4218c50f27d9f5a
Pema Zompa <pemazompa@hotmail.com>
2023-06-19 05:37:06 +0000

    Add view for Comment model

--- hacks/views.py
+++ hacks/views.py
@@ -1,9 +1,11 @@
 from django.urls.base import reverse
 from django.views.generic import CreateView
 from .models import Hack
+from .models import Comment
 from django import forms
 from .forms import HackForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class CreateHack(LoginRequiredMixin, CreateView):
     model = Hack
@@ -16,15 +18,15 @@ class CreateHack(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-# class CommentForm(forms.ModelForm):
-#     """ Create Comment Form """
-#     def __init__(self, *args, **kwargs):
-#         super(CommentForm, self).__init__(*args, **kwargs)
-#         self.fields['body'].widget = forms.Textarea(attrs={'rows': 3})
+class Comment(LoginRequiredMixin, CreateView):
+    
+    model = Comment
+    template_name = 'hacks/comment.html'
+    fields = ['comment_text', 'author']
+    success_url = '/hacks/'
 
-#     class Meta:
-#         """Get comment model, choose fields to display"""
-#         model = Comment
-#         fields = ('body',)
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
 
-  
+
