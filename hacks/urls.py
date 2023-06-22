3a9c99437bad6390eee6090d57c12210eb0692c4
Pema Zompa <pemazompa@hotmail.com>
2023-06-22 10:36:43 +0000

    Add hack templates and add urls

--- hacks/urls.py
+++ hacks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateHack
+from .views import CreateHack, Hacks
 
 urlpatterns = [
     path('', CreateHack.as_view(), name='create_hack'),
+    path('hacks/', Hacks.as_view(), name= 'hacks'),
+   
 ]
