7099178f629387af6c6bafe6862dcf493dd5586a
Pema Zompa <pemazompa@hotmail.com>
2023-06-25 18:57:24 +0000

    Create a template, updated view and url for Comment feature

--- hacks/urls.py
+++ hacks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import (
     CreateHack, Hacks,
     HackDetail, Comment, 
-    DeleteHack,
-    EditHack)
+    DeleteHack, EditHack, 
+    CreateComment)
 
 urlpatterns = [
     path("", CreateHack.as_view(), name="create_hack"),
@@ -11,4 +11,7 @@ urlpatterns = [
     path("<slug:pk>/", HackDetail.as_view(), name="hack_detail"),
     path("delete/<slug:pk>/", DeleteHack.as_view(), name="delete_hack_confirm"),
     path("edit/<slug:pk>/", EditHack.as_view(), name="edit_hack"),
+    path('create_comment/', CreateComment.as_view(), name='create_comment'),
+    path('create_comment/<int:hack_id>/', CreateComment.as_view(), name='create_comment'),
 ]
+
--- hacks/views.py
+++ hacks/views.py
@@ -9,35 +9,41 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .forms import HackForm
-from .models import Hack, Comment, Favorite
+from .forms import HackForm, CommentForm
+from .models import Hack, Comment
 
-
+"""Add a Hack"""
 
 class CreateHack(LoginRequiredMixin, CreateView):
     model = Hack
     template_name = "hacks/create_hack.html"
     fields = ["category", "title", "ingredients", "description", "image"]
-    success_url = "/hacks/"
+    success_url = "/hacks/hacks/"
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
+"""Create a comment"""
 
-class Comment(LoginRequiredMixin, CreateView):
+class CreateComment(LoginRequiredMixin, CreateView):
     model = Comment
-    template_name = "hacks/comment.html"
-    fields = ["comment_text", "author"]
-    success_url = "/hacks/"
+    template_name = "hacks/create_comment.html"
+    fields = ["comment_text"] 
 
     def form_valid(self, form):
-        form.instance.created_by = self.request.user
+        form.instance.creator = self.request.user
+        form.instance.name = self.request.user.username 
+        form.instance.email = self.request.user.email 
+        form.instance.hack = get_object_or_404(Hack, pk=self.kwargs['hack_id'])
         return super().form_valid(form)
+
+    def get_success_url(self):
+        hack_id = self.object.hack.id
+        return reverse('hack_detail', args=[hack_id])
 
 
 """View all hacks"""
-
 
 class Hacks(ListView):
     model = Hack
@@ -56,17 +62,35 @@ class Hacks(ListView):
             hacks = self.model.objects.all()
         return hacks
 
+"""View a hack"""
+
 class HackDetail(DetailView):
     model = Hack
     template_name = "hacks/hack_detail.html"
     context_object_name = "hack"
 
- 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.filter(active=True)
+        context['new_comment'] = None
+        context['comment_form'] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        hack = self.get_object()
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.hack = hack
+            new_comment.save()
+        return self.get(request, *args, **kwargs)
+
+
 """Delete hack"""
 
 class DeleteHack(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Hack
-    success_url = '/hacks/'
+    success_url = '/hacks/hacks/'
 
     def test_func(self):
         return self.request.user == self.get_object().created_by
@@ -77,11 +101,8 @@ class EditHack(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name= 'hacks/edit_hack.html'
     model = Hack
     form_class = HackForm
-    success_url = '/hacks/'
+    success_url = '/hacks/hacks/'
 
     def test_func(self):
         return self.request.user == self.get_object().created_by
-
-
-
 
