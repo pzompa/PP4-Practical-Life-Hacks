fc406fc17083c3914bb628dabb725c7169026cc7
Pema Zompa <pemazompa@hotmail.com>
2023-07-01 15:15:39 +0000

    Add toggle feature for like and favorite button

---
+++ hacks/templatetags/hack_tags.py
@@ -0,0 +1,25 @@
+from django import template
+from hacks.models import Like
+
+register = template.Library()
+
+@register.simple_tag
+def is_favorited_by_user(hack, user):
+    if user.is_authenticated:
+        return hack.favorite_set.filter(user=user).exists()
+    else:
+        return False
+
+@register.simple_tag
+def get_favorite(hack, user):
+    if user.is_authenticated:
+        return Favorite.objects.filter(hack=hack, user=user).first()
+    else:
+        return False
+
+@register.simple_tag
+def is_liked_by_user(hack, user):
+    if user.is_authenticated:
+        return Like.objects.filter(hack=hack, user=user).exists()
+    else:
+        return False
